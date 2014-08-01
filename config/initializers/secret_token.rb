2cc0413203249738f1dddfe155bb2f40a24f4198
tboneyang <tboneyang@gmail.com>
2014-08-01 11:57:01 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TabHaven::Application.config.secret_key_base = 'c9536205792460fb10ada74e4a310074078907f716783d48ad3e9f6690cb4469d0d0ec0568cd92fd130fd4a008e3e8a532a0f080de10d5cc4803fcee1dfb0c3a'
